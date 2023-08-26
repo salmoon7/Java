01261816a4227bc21e6a7c341c46242b9e980b7b
Adebayo <taofeekabdussalam07       @gmail.com>
2023-08-25 21:41:17 -0700

    THe TestMax. java is updated

--- TestMax.java
+++ TestMax.java
@@ -7,8 +7,7 @@ public static void main(String []aargs) {
 	int k=max(i,j);
 	
 	System.out.println("The maximum number between "+i+"and "+j+"is "+k);
-	
-	
+	System.out.println("Once you know Something it becomes Simple Trust me!!!");	
 }
 public static int max(int i,int j) {
 	int num=0;
